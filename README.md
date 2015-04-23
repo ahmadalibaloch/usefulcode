7bb2f806039ffc33dbceeb3687344ec10e2f4dd3
Ahmad Ali <ahmadalibaloch@gmail.ccom>
2015-04-23 12:57:53 +0500

    added license and readme

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # usefulcode
 Useful code related to anything.
+
+#####Its a project for easy copy past of the code mostly related to front end development
+###**List of codes**
+1. [Ng-Table to Odata query convertor](https://github.com/ahmadalibaloch/usefulcode/blob/master/ngtable_with_odata_utils.js)
--- ngtable_with_odata_utils.js
+++ ngtable_with_odata_utils.js
@@ -1,3 +1,11 @@
+/* 
+ * Copyright (C) 2015 Ahmad Ali
+ * http://about.me/ahmadalibaloch, ahmadalibaloch@gmail.com
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+
 angular.module('ahmadalibaloch')
 .factory('utils', ['$q', '$resource', , function ($q, $resource) {
 
